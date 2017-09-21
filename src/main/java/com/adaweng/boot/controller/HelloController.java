866786abdc5ba7612cd2465a0569a65bceff0cd0
eyaweng <ada.weng@ericsson.com>
2017-09-21 14:55:02 +0800

    Thymeleaf: return value to view

---
+++ src/main/java/com/adaweng/boot/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.adaweng.boot.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by eyaweng on 9/21/2017.
+ */
+@Controller
+@PropertySource("classpath:application.properties")
+public class HelloController {
+
+    @Value("${app.userName}")
+    private String  userName;
+
+    @Value("${app.title}")
+    private String title;
+
+    @RequestMapping("/")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView("welcome");
+        mv.addObject("name", userName);
+        mv.addObject("bookTitle", title);
+        return mv;
+    }
+
+}
